c642e1a1456cbabf0176ae6ede1c120c9439f558
rupesh-arch <2311cs020081@mallareddyuniversity.ac.in>
2025-03-04 11:34:53 +0530

    Add files via upload

---
+++ iris_deplyment.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[41]:
+
+
+import streamlit as st
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import io 
+from sklearn import tree
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+
+# In[11]:
+
+
+data = load_iris()
+df = pd.DataFrame(data.data, columns=data.feature_names)
+df['target'] = data.target
+df
+
+
+# In[19]:
+
+
+X = df.drop(columns=['target'])
+y = df['target']
+
+
+# In[21]:
+
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=42)
+clf = DecisionTreeClassifier()
+clf.fit(X_train, y_train)
+
+
+# In[25]:
+
+
+predictions = clf.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
+
+
+# In[43]:
+
+
+# Streamlit UI
+st.title("Decision Tree Classifier - Iris Dataset")
+st.write(f"Model Accuracy: {accuracy:.2f}")
+st.sidebar.header("Input Features")
+sepal_length = st.sidebar.slider("Sepal Length", float(df.iloc[:, 0].min()), float(df.iloc[:, 0].max()), float(df.iloc[:, 0].mean()))
+sepal_width = st.sidebar.slider("Sepal Width", float(df.iloc[:, 1].min()), float(df.iloc[:, 1].max()), float(df.iloc[:, 1].mean()))
+petal_length = st.sidebar.slider("Petal Length", float(df.iloc[:, 2].min()), float(df.iloc[:, 2].max()), float(df.iloc[:, 2].mean()))
+petal_width = st.sidebar.slider("Petal Width", float(df.iloc[:, 3].min()), float(df.iloc[:, 3].max()), float(df.iloc[:, 3].mean()))
+
+
+# In[47]:
+
+
+data_input = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+prediction = clf.predict(data_input)
+predicted_class = data.target_names[prediction[0]]
+
+st.subheader("Prediction")
+st.write(f"Predicted Class: **{predicted_class}**")
+
+
+# In[49]:
+
+
+if st.button("Show Decision Tree Diagram") :
+    fig, ax = plt.subplots(figsize=(12, 8))
+    tree.plot_tree(clf, feature_names=dta.feature_names, class_names=dta.target_names, filled=True, ax=ax)
+    buf = io.BytesIO()
+    plt.savefig(buf, format="png")
+    buf.seek(0)
+    st.images(buf, caption="Decision Tree Visualization", use_column_width=True)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
